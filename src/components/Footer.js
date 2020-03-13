2037f9096a8a254a077bb898d303c6b308a6e9ee
Lars Urban <urban@u-php.de>
2020-03-13 17:09:58 +0100

    style sheet änderungen

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,12 +12,15 @@ export default class Footer extends React.Component {
             <div className="footer">
                 <div className="foot_int">
                     <p><a href="https://www.u-php.de">Lars Urban / U-PHP</a></p>
+                    <hr />
                 </div>
                 <div className="foot_int">
                     <p><a href="https://www.u-php.de/datenschutz">Datenschutz</a></p>
+                    <hr />
                 </div>
                 <div className="foot_int">
                     <p><a href="https://www.u-php.de/impressum">Impressum</a></p>
+                    <hr />
                 </div>
             </div>
         )
